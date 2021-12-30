ec37aa3f4791a8b770e4c7517ff0f7c74702b8b0
Alex Dean <alex@zanconsulting.com>
2021-12-30 16:35:08 -0700

    BasePage - moved "loadPageRecord" functionality into Zan.data.ViewModel and make it compatible with links

--- src/page/BasePage.js
+++ src/page/BasePage.js
@@ -1,5 +1,8 @@
 Ext.define('Zan.ui.page.BasePage', {
     extend: 'Ext.panel.Panel',
+    requires: [
+        'Zan.data.ViewModel',
+    ],
 
     alias: 'widget.zan-page',
 
@@ -21,17 +24,11 @@ Ext.define('Zan.ui.page.BasePage', {
 
         // todo: docs
         zanRouteParameters: null,
-
-        // todo: docs
-        loadRecords: {},
-
-        // todo: docs
-        // todo: can this be replaced with "Model Links"? https://docs.sencha.com/extjs/7.5.0/classic/Ext.app.ViewModel.html
-        // todo: this feature would make more sense on a child class of ViewModel
-        loadPageRecord: null,
     },
 
-    viewModel: {},
+    viewModel: {
+        xclass: 'Zan.data.ViewModel',
+    },
 
     title: 'Loading...',
 
@@ -50,81 +47,8 @@ Ext.define('Zan.ui.page.BasePage', {
         this._applyRouteParametersToViewModel();
     },
 
-    afterRender: function() {
-        this.callParent();
-
-        setTimeout(Ext.bind(function () {
-            this._loadRecords();
-        }, this), 1);
-
-        setTimeout(Ext.bind(function () {
-            this._loadPageRecord();
-        }, this), 1);
-    },
-
     _applyRouteParametersToViewModel() {
-        this.lookupViewModel().set('zanRouteParameters', this.getZanRouteParameters());
-    },
-
-    async _loadPageRecord() {
-        var viewModel = this.lookupViewModel();
-        var routeParams = this.getZanRouteParameters();
-
-        if (!viewModel) this.setViewModel(Ext.create('Ext.app.ViewModel'));
-
-        // Nothing to do unless there's a pageRecord configuration
-        if (!this.getLoadPageRecord()) return;
-
-        var loadPageRecord = this.getLoadPageRecord();
-        var identifier = loadPageRecord.identifier;
-        if (loadPageRecord.identifierParam) {
-            identifier = routeParams[loadPageRecord.identifierParam];
-        }
-
-        // Merge identifier into additional parameters
-        var params = {};
-        if (loadPageRecord.hasOwnProperty('params')) {
-            params = loadPageRecord.params.__proto__;
-        }
-
-        params.identifier = identifier;
-
-        // Support additional responseFields from the API
-        if (!Ext.isEmpty(loadPageRecord.responseFields)) {
-            params.responseFields = loadPageRecord.responseFields;
-        }
-
-        var record = Ext.create(loadPageRecord.type);
-        record.load({
-            params: params,
-            success: function(record, op) {
-                viewModel.set('pageRecord', record);
-            }
-        });
-    },
-
-    async _loadRecords() {
-        var session = this.lookupSession();
-        var viewModel = this.lookupViewModel();
-        var routeParams = this.getZanRouteParameters();
-
-        // Nothing to do if no records were requested
-        if (Ext.Object.isEmpty(this.getLoadRecords())) return;
-
-        if (!session) throw new Error("Page must have a session declared in order to use loadRecords");
-
-        Ext.Object.each(this.getLoadRecords(), async function(viewModelName, loadInfo) {
-            // todo: better error detection/reporting here
-            var identifier = routeParams[loadInfo.identifierParam];
-            var extraParams = {};
-
-            if (!Ext.isEmpty(loadInfo.responseFields)) {
-                extraParams.responseFields = loadInfo.responseFields;
-            }
-
-            var record = await session.getRecordByAnyIdentifier(loadInfo.type, identifier, extraParams);
-            viewModel.set(viewModelName, record);
-        }, this);
+        this.lookupViewModel().set('zanRouteParams', this.getZanRouteParameters());
     },
 
     setTitle: function(value) {
